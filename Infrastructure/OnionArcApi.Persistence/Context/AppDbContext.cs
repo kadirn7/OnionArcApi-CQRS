8759b7ae1fc11c91fde57c3b76127deb86a55ab6
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-05-07 20:26:20 +0300

    Control

---
+++ Infrastructure/OnionArcApi.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnionArcApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArcApi.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Detail> Details { get; set; }  
+        public DbSet<Category> Categories { get; set; } 
+        public DbSet<Product> Products { get;set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+        }
+
+
+    }
+}
